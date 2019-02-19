095941f98b01bf58cc22a805e95b8cb290a7c9fb
gattinenirohini66 <46953862+gattinenirohini66@users.noreply.github.com>
2019-02-19 16:48:17 +0530

    Update ConsecutiveNumbers.java

--- main/java/com/stackroute/PE3/ConsecutiveNumbers.java
+++ main/java/com/stackroute/PE3/ConsecutiveNumbers.java
@@ -8,7 +8,6 @@ public class ConsecutiveNumbers {
         String[] array= values.split(",");
         for(int i=0;i<array.length-1;i++){
             int difference=Integer.parseInt(array[i+1])-Integer.parseInt(array[i]);
-            System.out.println(difference);
             if(difference==1 || difference==-1){
                 result=true;
             }
